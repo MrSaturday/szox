41e4b78ba04b66ae72ce73065098a5596ba14134
Sajmon <szymon.sobocinski@gmail.com>
2015-03-12 21:04:08 +0100

    project initialization with cmake + gtest

---
+++ source/szox.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Some day I'll be a cool app" << std::endl;
+}
+
---
+++ source/ut/ut.cc
@@ -0,0 +1,7 @@
+#include "gtest.h"
+
+TEST(simple, trial)
+{
+    EXPECT_EQ(4, 2+2);
+}
+
